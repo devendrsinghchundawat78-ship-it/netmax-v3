981494ca587789dfd8d24da11e3defcdc0d9dafe
devendrsinghchundawat78-ship-it <274628004+devendrsinghchundawat78-ship-it@users.noreply.github.com>
2026-09-15 22:04:20 +0000

    fix: add missing collectAsStateWithLifecycle import in AppShellComponents

--- composeApp/src/commonMain/kotlin/com/nuvio/app/AppShellComponents.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/AppShellComponents.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
